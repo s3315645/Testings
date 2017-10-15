605c3428365271502b7d1d1faab5b24419adeebf
s3315645 <31922684+s3315645@users.noreply.github.com>
2017-10-15 18:39:15 +1100

    Create novel.js

---
+++ novel.js
@@ -0,0 +1,4 @@
+/* We can stick all the novel objects in here;
+or we can make a folder and build them as individual files to import (which is what we'd do if we had a million files to manage)
+it's probably easier to just locate them all in here, 
+Maybe we can add that to questions to ask Sarah for thursday */
