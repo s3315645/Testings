e5acf3b68f888f57409fa7b58896fab5af9bf4a8
Kathrina <31477224+kjmaglanque@users.noreply.github.com>
2017-10-23 21:07:41 +1100

    Update display.js

--- javascript/display.js
+++ javascript/display.js
@@ -1,18 +1,38 @@
-
-function displayText(speakerName, textContent) {
+this 'function displayText(speakerName, textContent) {
     this.speakerName = speakerName;
     this.textContent = textContent;
+    
+    this.color = "#000000";
+    this.position = new Position(0, 0, true);
+    this.align = "left";
+    this.font = '20px "Deja Vu Sans", Helvetica, Arial, sans-serif';
+    this.width = 1.0; // decimal percentage
+    this.visibility = "visible";
 }
 
 
-function displayImage(imageName, imagePath) {
+function displayImage(imageName, imageSetting) {
     this.imageName = imageName;
-    this.imagePath = imagePath;
+    this.imageSetting = imageSetting;
+    
+    this.imageSRC = ""; /*directory where the image is located*/
+    
+    switch (imageSetting) {
+        case 'background':
+            this.alpha = 1.0; /*display as Opaque*/
+            break;
+        case 'foreground':
+            this.alpha = .07;
+            break;
+        default:
+    }
+    
 }
 
 
 function displayAudio(audioName) {
     this.audioName = audioName;
+    this.audioSRC = "";
 }
 
 
