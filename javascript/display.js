ca075a998e408466870101d408be19fe5155c1be
Kathrina <31477224+kjmaglanque@users.noreply.github.com>
2017-10-23 20:17:05 +1100

    Update display.js

--- javascript/display.js
+++ javascript/display.js
@@ -1,1 +1,22 @@
 
+function displayText(speakerName, textContent) {
+    this.speakerName = speakerName;
+    this.textContent = textContent;
+}
+
+
+function displayImage(imageName, imagePath) {
+    this.imageName = imageName;
+    this.imagePath = imagePath;
+}
+
+
+function displayAudio(audioName) {
+    this.audioName = audioName;
+}
+
+
+function displayDecision(button1, button2) {
+    this.button1 = buton1;
+    this.button2 = button2;
+}
