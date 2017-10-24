4216f52e8cbea49e20cb4b4f916c4c667f72cb65
Kathrina Maglanque <s3343129@student.rmit.edu.au>
2017-10-24 19:38:54 +1100

    sync

--- javascript/display.js
+++ javascript/display.js
@@ -1,7 +1,7 @@
-this 'function displayText(speakerName, textContent) {
+function displayText(speakerName, textContent) {
     this.speakerName = speakerName;
     this.textContent = textContent;
-    
+
     this.color = "#000000";
     this.position = new Position(0, 0, true);
     this.align = "left";
@@ -14,19 +14,28 @@ this 'function displayText(speakerName, textContent) {
 function displayImage(imageName, imageSetting) {
     this.imageName = imageName;
     this.imageSetting = imageSetting;
-    
+
     this.imageSRC = ""; /*directory where the image is located*/
-    
+
     switch (imageSetting) {
         case 'background':
-            this.alpha = 1.0; /*display as Opaque*/
+            this.alpha = 0.7; /*display as Opaque*/
+
             break;
-        case 'foreground':
-            this.alpha = .07;
+
+        case 'left':
+            this.alpha = 1.0;
+            this.style ="position:left";
             break;
+
+        case 'right':
+            this.alpha = 1.0;
+            this.style = "position:right";
+          break;
+
         default:
     }
-    
+
 }
 
 
@@ -39,4 +48,6 @@ function displayAudio(audioName) {
 function displayDecision(button1, button2) {
     this.button1 = buton1;
     this.button2 = button2;
+
+
 }
