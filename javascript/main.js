cb04b1a92a8cca3ec4a9b1ab61e1ade18971d8a0
s3315645 <31922684+s3315645@users.noreply.github.com>
2017-10-25 14:41:35 +1100

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -1,4 +1,4 @@
-main(){    
+   
 	const novel = require('novel.js');
 	const display = require('display.js');
 	/*const timer = require('timer.js');
@@ -15,20 +15,25 @@ main(){
 	
 	function intro(){
 	
-	display.displayImage('introIMG.jpg', 'background');
+	display.displayImage('title.jpg', 'background');
 	display.displayAudio('theme.mp3');
 	display.displayText('','Click anywhere to begin');
 		
 	}
 	
+	
 	function playScene(sceneObject){
 		this.scene = sceneObject;
-		for(int i = 0; i < scene.lines.length; i++){ //This will actually be a callback loop, the for loop is only temporary.
+		
+		let i = 0;
+
+		while(i < scene.lines.length()){ 
 			if(scene.lines[i].getBGIMG() !== ''){ display.displayImage(scene.lines[i].getBGIMG(), 'background'); }
 			if(scene.lines[i].getleftimg() !== ''){ display.displayImage(scene.lines[i].getLeftIMG(), 'left'); }
 			if(scene.lines[i].getrightimg() !== ''){ display.displayImage(scene.lines[i].getRightIMG(), 'right'); }
 			if(scene.lines[i].getaudio() !== ''){ display.displayAudio(scene.lines[i].getAudio()); }
 			display.displayText(scene.lines[i].getSpeakerName(), scene.lines[i].getSceneText()
+			document.getElementById("catchclick").addEventListener("click", i++);
 		}	
 		return display.displayDecision(scene.decision1(), scene.decision2());
 	}
@@ -38,7 +43,7 @@ main(){
 	intro();
 	do{
 		currentScene = playScene(currentScene);
+		/*Automatic Savegame?*/ 
 	}while(currentScene.getFileName() != /*end scene key*/) 
 	
 	
-}
