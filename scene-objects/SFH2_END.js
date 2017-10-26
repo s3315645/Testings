529eb49d9390f50d5758d222c6924cbdb0b4ca01
Kathrina <31477224+kjmaglanque@users.noreply.github.com>
2017-10-26 22:04:34 +1100

    Create SFH2_END.js
    Array for Spider Fan's House (2) Ending

---
+++ scene-objects/SFH2_END.js
@@ -0,0 +1,102 @@
+"lines": [
+		{
+            "speaker_name": "Walter",
+            "dialogue": "Mr Catumiri, the jig is up.",
+        },
+        {
+            "speaker_name": "Mr Catumiri",
+            "dialogue": "I’m sorry, what?"
+        },
+		{
+            "speaker_name": "Walter",
+            "dialogue": "What have you done with the detective?"
+        },
+		{
+            "speaker_name": "Mr Catumiri",
+            "dialogue": "I have no idea what you’re talking about."
+        },
+		{
+            "speaker_name": "Walter",
+            "dialogue": "Don’t play dumb with me, Catumiri, where have you put him? Have you got your spiders to tie him up with webs?"
+        },
+		{
+            "speaker_name": "Mr Catumiri",
+            "dialogue": "How dare you accuse my spiders of engaging in foul play, they would never do such a thing!"
+        },
+		{
+            "speaker_name": "Walter",
+            "dialogue": "You’ve had it in for the detective ever since you two argued over the existence of a 10 legged spider."
+        },
+		{
+            "speaker_name": "Mr Catumiri",
+            "dialogue": "Ahhhhh, how many times do I have to explain this...the front two appendages were not used for walking and are therefore clearly not legs, THEY WERE PALPI!!!!"
+        },
+		{
+            "speaker_name": "Walter",
+            "dialogue": "You seem very upset about this Mr Catumiri, I think it’s time to call the police."
+        },
+		{
+            "speaker_name": "Mr Catumiri",
+            "dialogue": "NO! That incompetent Constable has it in for me, he’ll fix it so I’m locked up for good! Then who will look after my babies? Charlotte Porcino? She’ll accidentally kill them all within a month!! You’ll do no such thing."
+        },
+		{
+            "speaker_name": "Walter",
+            "dialogue": "Well Catumiri, you’ve left me with no choice, excuse me."
+        },
+		{
+            "speaker_name": "Mr Catumiri",
+            "dialogue": "No, sir, it is you who have left me with no choice."
+        },
+		{
+            "speaker_name": "Mr Catumiri",
+            "dialogue": "*Mr Catumiri clocks Walter over the head*"
+        },
+		{
+            "speaker_name": "Walter",
+            "dialogue": "*Walter wakes up*"
+        },
+		{
+            "speaker_name": "Walter",
+            "dialogue": "Wha-what happened? Where am I? I can’t move, why am I tied up? Why am I tied up with...spider webs?? "
+        },
+		{
+            "speaker_name": "Mr Catumiri",
+            "dialogue": "You do understand that I can’t let you leave now, sir."
+        },
+		{
+            "speaker_name": "Walter",
+            "dialogue": "Now, Mr Catumiri, perhaps I was somewhat rash - "
+        },
+		{
+            "speaker_name": "Mr Catumiri",
+            "dialogue": "You should have listened to me. I didn’t kidnap that stupid detective, but now you’ve forced my hand to something much, much worse. I will not going to prison, whatever the cost."
+        },
+		{
+            "speaker_name": "Walter",
+            "dialogue": "*gulps* You’re insane, Catumiri."
+        },
+		{
+            "speaker_name": "Mr Catumiri",
+            "dialogue": "Enough of this. Where are you Eva? There you are."
+        },
+		{
+            "speaker_name": "Walter",
+            "dialogue": "Eva?"
+        },
+		{
+            "speaker_name": "Mr Catumiri",
+            "dialogue": "My funnel web. This will not be at all pleasant for you, sir, but her poison will only take a few minutes to kill you."
+        },
+		{
+            "speaker_name": "Walter",
+            "dialogue": "To kill me??? Catumiri what are you doing!!!!!"
+        },
+		{
+            "speaker_name": "Mr Catumiri",
+            "dialogue": "Now, Eva, bite the snooping apprentice’s hand, that’s it….and we’ll just pretend this was all an accident."
+        },
+		{
+            "speaker_name": "Walter",
+            "dialogue": "Walter: Aaaaaaaaaah….noooooooooooooooooooooo!!!!!!!!!!!!"
+        },
+		]
