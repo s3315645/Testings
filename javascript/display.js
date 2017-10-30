6f41ff50ea016ddab086d684bbc8f8b5a6bd922b
Kathrina <31477224+kjmaglanque@users.noreply.github.com>
2017-10-30 18:57:25 +1100

    Update display.js

--- javascript/display.js
+++ javascript/display.js
@@ -26,8 +26,6 @@ function displayImage(imageName, imageSetting) {
     this.imageName = imageName;
     this.imageSetting = imageSetting;
 
-    this.imageSRC = ""; /*directory where the image is located*/
-
     switch (imageSetting) {
 
         case 'background':
@@ -56,25 +54,14 @@ function displayAudio(audioName) {
 
 
 function displayDecision(button1, button2) {
-    this.button1 = button1;
-    this.button2 = button2;
+  document.getElementbyId("button1").innerHTML = "button1";
+  document.getElementbyId("button2").innerHTML = "button2";
 }
 
 const debug = true;
 
 	if(debug){
-		document.getElementById('textBox').innerHTML =  "Debug testing";
-		document.getElementById('textBox').innerHTML = "Testing background";
-		displayImage('debug\debugBG.jpg', 'background');
-		document.getElementById('textBox').innerHTML = "Testing Right Sprite";
-		displayImage('debug\debugLS.png', 'right');
-		document.getElementById('textBox').innerHTML = "Testing Left Sprite";
-		displayImage('debug\debugRS.png', 'left');
-		document.getElementById('textBox').innerHTML = "Testing Audio";
-		displayAudio('debug\debug.mp3');
-		document.getElementById('textBox').innerHTML = "Testing Text";
-		displayText('Debug', 'displayText is working!');
-		document.getElementById('textBox').innerHTML = "Testing decision";
-		//displayDecision();
-		document.getElementById('textBox').innerHTML = "Testing Finished";
+		document.getElementById('speaker').innerHTML = "Walter:";
+    		document.getElementById('dialogue').innerHTML = "I have a feeling today's going to be a great day.";
+
 	}
